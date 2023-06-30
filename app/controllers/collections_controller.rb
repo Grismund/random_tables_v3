2392084f9ec3356f73b8fa54d57d19c7a0b3df8a
Seth Faerber <seth.faerber@ramseysolutions.com>
2023-06-30 10:38:27 -0500

    renders the partial with terms

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,6 +1,7 @@
 class CollectionsController < ApplicationController
   def index
     @collections = Collection.all
+    @selected_collection = Collection.find(params[:collection_id]) if params[:collection_id].present?
   end
 end
 
